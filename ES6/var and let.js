eb019642286e7ad041946b34559388b1a3da4064
Dario Kemenovic <dkemenovic@gmail.com>
2019-05-20 00:02:13 +0200

    update var and let

--- ES6/var and let.js
+++ ES6/var and let.js
@@ -2,7 +2,7 @@
 Explore Differences Between the var and let Keywords
 One of the biggest problems with declaring variables with the var keyword is that you can overwrite variable declarations without an error.
 
-var camper = 'James';
+var camper = 'James'; 
 var camper = 'David';
 console.log(camper);
 // logs 'David'
