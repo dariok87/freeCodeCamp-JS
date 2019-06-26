fc6f7e2d3d5326ec6b453e5fd0d00b4049aa458a
Dario Kemenovic <dkemenovic@gmail.com>
2019-06-26 23:58:59 +0200

    Refactor into ES6

--- Object Oriented Programming/03 Create a Method on an Object.js
+++ Object Oriented Programming/03 Create a Method on an Object.js
@@ -22,7 +22,7 @@ let dog = {
   name: "Spot",
   numLegs: 4,
   sayLegs: function() {
-    return "This dog has " + dog.numLegs + " legs.";
+    return `This dog has ${dog.numLegs} legs.`;
   }
 };
 
