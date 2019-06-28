25285fcbcad0e8db186f8747106837ac5eb1d652
Dario Kemenovic <dkemenovic@gmail.com>
2019-06-28 22:38:13 +0200

    Change to ES6

--- Object Oriented Programming/04 Make Code More Reusable with the this Keyword.js
+++ Object Oriented Programming/04 Make Code More Reusable with the this Keyword.js
@@ -23,7 +23,7 @@ Modify the dog.sayLegs method to remove any references to dog. Use the duck exam
 let dog = {
   name: "Spot",
   numLegs: 4,
-  sayLegs: function() {return "This dog has " + this.numLegs + " legs.";}
+  sayLegs: function() {return `This dog has ${this.numLegs} legs.`;}
 };
 
 dog.sayLegs();
