564d9a2c168de2f239f6216d0b59a802e632586f
Dario <dkemenovic@gmail.com>
2019-09-25 23:37:13 +0200

    Refactor code

--- Functional Programming/06 Refactor Global Variables Out of Functions.js
+++ Functional Programming/06 Refactor Global Variables Out of Functions.js
@@ -8,46 +8,19 @@ So far, we have seen two distinct principles for functional programming:
 Adding one to a number is not very exciting, but we can apply these principles when working with arrays or more complex objects.
 
 
-Refactor (rewrite) the code so the global array bookListis not changed inside either function. The addfunction should add the given bookNameto the end of an array. The removefunction should remove the given bookNamefrom an array. Both functions should return an array, and any new parameters should be added before the bookNameone.
+Refactor (rewrite) the code so the global array bookList is not changed inside either function. The add function should add the given bookNameto the end of an array. The removefunction should remove the given bookNamefrom an array. Both functions should return an array, and any new parameters should be added before the bookNameone.
 */
 
-// the global variable
-var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
-
-/* This function should add a book to the list and return the list */
-// New parameters should come before the bookName one
-
-// Add your code below this line
-// the global variable
-var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
-
-/* This function should add a book to the list and return the list */
-// New parameters should come before the bookName one
-
-// Add your code below this line
 function add (arr, bookName) {
-  let newArr = [...arr];  // Copy the bookList array to a new array.
-  newArr.push(bookName);  // Add bookName parameter to the end of the new array.
-  return newArr; // Return the new array.
-}
-
-function remove (arr, bookName) {
-  let newArr = [...arr];  // Copy the bookList array to a new array.
-  if (newArr.indexOf(bookName) >= 0) {   // Check whether the bookName parameter is in new array.
-    
-    newArr.splice(newArr.indexOf(bookName), 1); // Remove the given paramater from the new array.
+    let newArr = [...arr];  // Copy the bookList array to a new array.
+    newArr.push(bookName);  // Add bookName parameter to the end of the new array.
     return newArr; // Return the new array.
-    }
-}
-
-var newBookList = add(bookList, 'A Brief History of Time');
-var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
-var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
-
-console.log(bookList);
-
-var newBookList = add(bookList, 'A Brief History of Time');
-var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
-var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
-
-console.log(bookList);
+  }
+  
+  function remove (arr, bookName) {
+    let newArr = [...arr];  // Copy the bookList array to a new array.
+    if (newArr.indexOf(bookName) >= 0) {   // Check whether the bookName parameter is in new array.
+      newArr.splice(newArr.indexOf(bookName), 1); // Remove the given paramater from the new array.
+      return newArr; // Return the new array.
+      }
+  }
