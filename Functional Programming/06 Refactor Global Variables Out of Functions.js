3bf23ee96c0d9c70f009969cf1dcaefe224ee98b
Dario <dkemenovic@gmail.com>
2019-09-25 23:01:31 +0200

    Create Task 6

---
+++ Functional Programming/06 Refactor Global Variables Out of Functions.js
@@ -0,0 +1,11 @@
+/*
+So far, we have seen two distinct principles for functional programming:
+
+1) Don't alter a variable or object - create new variables and objects and return them if need be from a function.
+
+2) Declare function arguments - any computation inside a function depends only on the arguments, and not on any global object or variable.
+
+Adding one to a number is not very exciting, but we can apply these principles when working with arrays or more complex objects.
+
+
+Refactor (rewrite) the code so the global array bookListis not changed inside either function. The addfunction should add the given bookNameto the end of an array. The removefunction should remove the given bookNamefrom an array. Both functions should return an array, and any new parameters should be added before the bookNameone.*/
