041014e08616bee8fee277a625127118e1eab16c
Dario <dkemenovic@gmail.com>
2019-09-25 23:05:36 +0200

    Create Task 6

--- Functional Programming/06 Refactor Global Variables Out of Functions.js
+++ Functional Programming/06 Refactor Global Variables Out of Functions.js
@@ -8,4 +8,38 @@ So far, we have seen two distinct principles for functional programming:
 Adding one to a number is not very exciting, but we can apply these principles when working with arrays or more complex objects.
 
 
-Refactor (rewrite) the code so the global array bookListis not changed inside either function. The addfunction should add the given bookNameto the end of an array. The removefunction should remove the given bookNamefrom an array. Both functions should return an array, and any new parameters should be added before the bookNameone.*/
+Refactor (rewrite) the code so the global array bookListis not changed inside either function. The addfunction should add the given bookNameto the end of an array. The removefunction should remove the given bookNamefrom an array. Both functions should return an array, and any new parameters should be added before the bookNameone.
+*/
+
+// the global variable
+var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+/* This function should add a book to the list and return the list */
+// New parameters should come before the bookName one
+
+// Add your code below this line
+function add (bookName) {
+  
+  return bookList.push(bookName);
+  
+  // Add your code above this line
+}
+
+/* This function should remove a book from the list and return the list */
+// New parameters should come before the bookName one
+
+// Add your code below this line
+function remove (bookName) {
+  if (bookList.indexOf(bookName) >= 0) {
+    
+    return bookList.splice(0, 1, bookName);
+    
+    // Add your code above this line
+    }
+}
+
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+console.log(bookList);
