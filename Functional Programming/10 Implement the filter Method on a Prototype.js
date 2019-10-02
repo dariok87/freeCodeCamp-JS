0dbc3c1c4781cefffeffa70aa542b38d135a7979
Dario <dkemenovic@gmail.com>
2019-10-02 21:53:40 +0200

    Finish Task 10

--- Functional Programming/10 Implement the filter Method on a Prototype.js
+++ Functional Programming/10 Implement the filter Method on a Prototype.js
@@ -12,9 +12,11 @@ var s = [23, 65, 98, 5];
 
 Array.prototype.myFilter = function(callback){
   var newArray = [];
-  // Add your code below this line
-  
-  // Add your code above this line
+  this.forEach(function(x) {
+    if (callback(x) == true) {
+      newArray.push(x);
+    }
+  })  
   return newArray;
 
 };
