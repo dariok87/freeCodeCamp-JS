1d4177d3e39478fa74b939ebce00a866c9328d9c
Dario <dkemenovic@gmail.com>
2019-10-16 09:53:51 +0200

    Add another solution

--- Functional Programming/08 Implement map on a Prototype.js
+++ Functional Programming/08 Implement map on a Prototype.js
@@ -11,6 +11,26 @@ Note: A pure function is allowed to alter local variables defined within its sco
 Write your own Array.prototype.myMap(), which should behave exactly like Array.prototype.map(). You may use a for loop or the forEach method.
 */
 
+// #1 Solution:
+
+// the global Array
+var s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback) {
+  var newArray = [];
+  // Add your code below this line
+  this.forEach(a => newArray.push(callback(a)));
+  // Add your code above this line
+  return newArray;
+};
+
+var new_s = s.myMap(function(item) {
+  return item * 2;
+});
+
+
+// #2 Solution
+
 // the global Array
 var s = [23, 65, 98, 5];
 
