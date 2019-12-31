a925522217d11b5fe015bef03e540c268399607d
Dario <dkemenovic@gmail.com>
2019-12-31 18:21:22 +0100

    Solve Task 01

--- Debugging/Basic Javascript/01 Use Recursion to Create a Countdown.js
+++ Debugging/Basic Javascript/01 Use Recursion to Create a Countdown.js
@@ -21,7 +21,14 @@ We have defined a function called countdown with one parameter (n). The function
 */
 
 //Only change code below this line
+
 function countdown(n) {
-  return;
+  if (n < 1) {
+    return [];
+  } else {
+    const arr = countdown(n - 1);
+    arr.unshift(n);
+    return arr;
+  }
 }
 console.log(countdown(5)); // [5, 4, 3, 2, 1]
