e1389dfafdde3de249bc6d7a48602eef8eb7f81d
Dario <dkemenovic@gmail.com>
2020-01-11 18:00:38 +0100

    Add Task 01

---
+++ Intermediate Algorithm Scripting/01 Sum All Numbers in a Range.js
@@ -0,0 +1,13 @@
+/*
+We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them. The lowest number will not always come first.
+
+For example, sumAll([4,1]) should return 10 because sum of all the numbers between 1 and 4 (both inclusive) is 10.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+*/
+
+function sumAll(arr) {
+  return 1;
+}
+
+sumAll([1, 4]);
