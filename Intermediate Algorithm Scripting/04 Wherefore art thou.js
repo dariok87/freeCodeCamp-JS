b345731a9dfd24911da342bfb58468559ef935d5
Dario <dkemenovic@gmail.com>
2020-01-17 21:38:48 +0100

    Finish Task 04

--- Intermediate Algorithm Scripting/04 Wherefore art thou.js
+++ Intermediate Algorithm Scripting/04 Wherefore art thou.js
@@ -8,10 +8,14 @@ Remember to use Read-Search-Ask if you get stuck. Write your own code.
 
 function whatIsInAName(collection, source) {
   // What's in a name?
-  var arr = [];
-  // Only change code below this line
-
-  // Only change code above this line
+  var arr = collection.filter(function(item) {
+    for (var i in source) {
+      if (source[i] != item[i]) {
+        return false;
+      }
+    }
+    return true;
+  });
   return arr;
 }
 
